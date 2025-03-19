2549bf300f51b76925ad2425549564a3779e6b58
Lavnish212 <lavnish1255.becse24@chitkara.edu.in>
2025-03-19 15:43:10 +0530

    Solution to Set nth Bit of a Number

---
+++ Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n,position,i=0;
+    scanf("%d %d",&n,&position);
+    int arr[32];
+    while(n>0){
+        arr[i]=n%2;
+        n=n/2;
+        i++;
+    }
+    int sum=0;
+    int multiple=1;
+    int len=sizeof(arr)/sizeof(arr[0])
+    for(int j=0;j<len;j++){
+        if(j==position){
+            arr[j]=1
+        }
+        sum=sum+multiple*arr[j];
+        multiple=multiple*2;
+    }
+    printf("%d",sum);
+    return 0;
+}
